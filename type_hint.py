e23c2b07a11c82c1e5a49de217e39035952fab73
Douglas Sexton <sextondouglas@comcast.net>
2026-09-11 12:27:52 -0700

    idk why y pos is 208 in game seems to just be center screen

--- type_hint.py
+++ type_hint.py
@@ -6,14 +6,11 @@ class TypeHint(BaseModel):
     x_pos: int
     y_pos: int
 
-'''
-IMPORTANT: Actors in game go from Y -100 to 380 for some reason. So add 100 to shift to 0 to 480
-'''
 HINTS = {
     "walkup": TypeHint(
         name="walkup",
         desc="Batter walking up to the plate.",
         x_pos=320,
-        y_pos=308
+        y_pos=240
     )
 }
